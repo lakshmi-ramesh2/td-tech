4cd74927a5970ad0069b53a99cd8cdcead467c81
lakshmiraman83 <lakshmi83@yahoo.com>
2015-10-23 23:10:58 -0700

    Added functions to validate options, build and execute query

--- query.py
+++ query.py
@@ -1,23 +1,78 @@
 import os
+import sys, getopt
 import tdclient
-apikey = "7072/89472d81d6e3e172ab0eadd0cf89829ea572abca"
+
+#Validate options
+def validate_options(engine,format):
+	if engine not in ("presto", "hive"):
+		print "Please specify correct engine value"
+		sys.exit(2)
+	if format not in ("tabular", "csv"):
+		print "Please specify correct format value"
+		sys.exit(2)
+
+#Build query
+def build_query(col_list,table_name,min_time,max_time,limit):
+	query = "SELECT " + col_list + " FROM " + table_name + " where TD_TIME_RANGE(time," + min_time + "," + max_time + ")"
+	if limit is not None:
+		query = query + " limit " + limit
+	return query
+
+#Execute query
+def execute_query(apikey,db_name,query,engine,format):
+		with tdclient.Client(apikey) as client:
+    			job = client.query(db_name,query,type=engine)
+    			# sleep until job's finish
+    			job.wait()
+			f = open('results.txt', 'w')
+    			for row in job.result():
+				if(format == 'csv'):  			
+      					f.write(','.join(map(str, row)))
+				else:
+					f.write('\t'.join(map(str, row)))
+				f.write('\n')
 
 
-db_name = "airport_data"
-table_name = "sfo_pass_raw"
-col_list = "activity_type_code,operating_airline,geo_region,month,year,passenger_count"
-min_time = "1420099200"
-max_time = "1433142000"
-engine = "hive"
-format = "tabular"
-limit = "100"
+def main():
 
-query = "SELECT " + col_list + " FROM " + table_name + " where TD_TIME_RANGE(time," + min_time + "," + max_time + ") limit " + limit
-print query
+	apikey = "7072/89472d81d6e3e172ab0eadd0cf89829ea572abca"
+	db_name = ''
+	table_name = ''
+	col_list = '*'
+	min_time = None
+	max_time = None
+	engine = "presto"
+	format = "tabular"
+	limit = None
 
-with tdclient.Client(apikey) as client:
-    job = client.query(db_name, query,type=engine)
-    # sleep until job's finish
-    job.wait()
-    for row in job.result():
-        print(row)
+	try:
+        	myoptions, args = getopt.getopt(sys.argv[1:],"f:e:c:m:M:l:")
+	except getopt.GetoptError as err:
+		print "Please specify a valid option: -f, -e, -c, -m, -M, -l"
+        	sys.exit(2)
+
+	db_name = args[0]
+	table_name = args[1]
+
+	for o, a in myoptions:
+    		if o == '-f':
+        		format = a
+    		elif o == '-e':
+        		engine = a
+    		elif o == '-c':
+        		col_list = a
+    		elif o == '-m':
+        		min_time = a
+    		elif o == '-M':
+        		max_time = a
+    		elif o == '-l':
+        		limit = a
+
+	validate_options(engine,format)
+
+	query = build_query(col_list,table_name,min_time,max_time,limit)
+	print query
+	execute_query(apikey,db_name,query,engine,format)
+
+if __name__ == "__main__":
+    main()
